422a31fab21db7d72ba421fa61e1172a132955ff
kelton.zhou <kelton.zhou@temu.com>
2025-09-27 17:59:17 +0800

    macos build

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 # brew install mingw-w64
 
 CGO_ENABLED=1 GOOS=windows GOARCH=amd64 CC=x86_64-w64-mingw32-gcc go build -ldflags="-H=windowsgui" -o rollcall.exe
+
+
+/Users/zhouzekun/go/bin/fyne package -os darwin -icon icon.png
