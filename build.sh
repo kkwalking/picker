35dd1f7fe54900c06cfe9b3bfa81f76c5ca2b6ee
kelton.zhou <kelton.zhou@temu.com>
2025-10-02 19:59:55 +0800

    v4 without macos build

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@
 
 CGO_ENABLED=1 GOOS=windows GOARCH=amd64 CC=x86_64-w64-mingw32-gcc go build -ldflags="-H=windowsgui" -o rollcall.exe
 
-
-/Users/zhouzekun/go/bin/fyne package -os darwin -icon icon.png
+# build in macos
+/Users/zhouzekun/go/bin/fyne package -os darwin -icon icon.png --name picker
+sudo spctl --master-disable
+xattr -cr /Applications/picker.app
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/kkwalking/picker-v2
+module github.com/kkwalking/picker
 
 go 1.24.2
 
